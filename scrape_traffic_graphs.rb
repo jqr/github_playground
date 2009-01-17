77f4d70498161613b30822e1948490567fb86ada
Elijah Miller <elijah.miller@gmail.com>
2009-01-16 22:54:26 -0500

    Adding more stats and information to output page.

--- scrape_traffic_graphs.rb
+++ scrape_traffic_graphs.rb
@@ -14,8 +14,15 @@ ARGV.each do |login|
 
   user_data = HTTParty.get("http://github.com/api/v1/json/#{login}")['user']
   output_filename = File.join(File.dirname(__FILE__), 'tmp', "#{login}.html")
-
+  
   File.open(output_filename, 'w') do |f|
+    f.write("<html>")
+    f.write("<head><title>#{login}'s Project Stats</title></head>")
+    f.write("<body>")
+    f.write(%Q{<h1><a href="#{user_data['url']}">#{login}</a>'s Project Stats</h1>\n})
+    f.write(%Q{#{user_data['location']}, \n}) if user_data['location']
+    f.write(%Q{<a href="mailto:#{user_data['email']}">#{user_data['email']}</a><br />\n}) if user_data['email']
+
     repositories = [user_data['repositories']].flatten.compact
     repositories.each do |repository|
       project = repository['name']
@@ -23,9 +30,13 @@ ARGV.each do |login|
       page_data = HTTParty.get("http://github.com/#{login}/#{project}/graphs/traffic")
       page = Hpricot.parse(page_data)
       traffic_graph_img = (page / 'img[@alt="Google Chart"]').first
-      f.write("<h2>#{project}</h2>\n")
+      f.write(%Q{<h2><a href="#{repository['url']}">#{project}</a></h2>\n})
+      f.write("#{repository['watchers']} watchers, #{repository['forks']} forks<br />\n")
+      f.write("<p>#{repository['description']}</p>") if repository['description'].size > 0
       f.write(traffic_graph_img.to_s + "\n")
     end
+    f.write("</body>")
+    f.write("</html>")
   end
 
   system("open #{output_filename}")
