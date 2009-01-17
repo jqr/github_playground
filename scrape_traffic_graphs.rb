ac074c5388002763e12f541fceed76ce2d46a8e9
Elijah Miller <elijah.miller@gmail.com>
2009-01-16 22:12:27 -0500

    Fetching more than one user at a time and storing the HTML in a tmp dir.

--- scrape_traffic_graphs.rb
+++ scrape_traffic_graphs.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-unless ARGV.size == 2
-  puts "usage is scrape_traffic_graphs.rb <user> <output_file>"
+if ARGV.size < 1
+  puts "usage is scrape_traffic_graphs.rb <users>"
   exit(1)
 end
 
@@ -9,20 +9,24 @@ require 'rubygems'
 require 'httparty'
 require 'hpricot'
 
-login = ARGV[0]
+ARGV.each do |login|
 
-puts "fetching #{login}..."
+  puts "fetching #{login}'s projects"
 
-user_data = HTTParty.get("http://github.com/api/v1/json/#{login}")['user']
+  user_data = HTTParty.get("http://github.com/api/v1/json/#{login}")['user']
+  output_filename = File.join(File.dirname(__FILE__), 'tmp', "#{login}.html")
 
-File.open(ARGV[1], 'w') do |f|
-  user_data['repositories'].each do |repository|
-    project = repository['name']
-    puts "  fetching #{project}..."
-    page_data = HTTParty.get("http://github.com/#{login}/#{project}/graphs/traffic")
-    page = Hpricot.parse(page_data)
-    traffic_graph_img = (page / 'img[@alt="Google Chart"]').first
-    f.write("<h2>#{project}</h2>\n")
-    f.write(traffic_graph_img.to_s + "\n")
+  File.open(output_filename, 'w') do |f|
+    user_data['repositories'].each do |repository|
+      project = repository['name']
+      puts "  fetching #{project}..."
+      page_data = HTTParty.get("http://github.com/#{login}/#{project}/graphs/traffic")
+      page = Hpricot.parse(page_data)
+      traffic_graph_img = (page / 'img[@alt="Google Chart"]').first
+      f.write("<h2>#{project}</h2>\n")
+      f.write(traffic_graph_img.to_s + "\n")
+    end
   end
+
+  system("open #{output_filename}")
 end
